edad7cf429457de9e80c320b890d69f2e1f2d07e
gabrielgomez20132 <gabrielgomez_100@hotmail.com>
2022-02-11 12:05:56 -0300

    Alta del Rol

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -11,4 +11,20 @@ class RoleController extends Controller
     {       
         return  Role::get();
     }
+
+    public function store(Request $request){
+        $rol = new Role();
+        $rol->id = $request->get('id');
+        $rol->name = $request->get('name');
+        
+        $rol->save();
+
+        //log event//
+        //Log::channel('events')->info('Registered user: ip address: '.$request->ip().' | User id: '.$request->user().' | User id create: '.$user);
+
+        return response()->json([
+            'message' => 'Se ha creado el Rol correctamente',
+            'user' => $rol
+        ]);
+    }
 }
