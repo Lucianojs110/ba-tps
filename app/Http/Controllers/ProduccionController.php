cc58e71ffe82ad73cad4c49d183c94f9adec975b
gabrielgomez20132 <gabrielgomez_100@hotmail.com>
2022-03-17 12:52:23 -0300

    Inicio de endpoint produccion

--- app/Http/Controllers/ProduccionController.php
+++ app/Http/Controllers/ProduccionController.php
@@ -8,77 +8,62 @@ use Illuminate\Support\Facades\Log;
 
 class ProduccionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         return Produccion::with('producto')->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $produccion = new Produccion();
+        $stock = new Stock();
+
+
+        if(request('acciones') == 'procesar'){
+
+            
+
+            
+        }else{
+
+            $produccion->id_producto = request('id_producto');
+            $produccion->acciones = 'desactivar';
+            $produccion->cantidad = request('cantidad');
+            $produccion->estado = 'finalizado';
+            $produccion->save();
+
+        
+
+            //log event//
+            Log::channel('events')->info('Ingreso nueva Produccion: ip address: '.$request->ip().
+                                        ' | Usuario id: '.$request->user()->id.
+                                        ' | Ingreso: ' .$produccion);
+
+            $Res = Produccion::findorFail($produccion->id);
+        }
+        
+
+        return response()->json([
+            'message' => 'Se ha creado la Produccion correctamente',
+            'Produccion' => $Res
+        ]);
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
-        //
+        return  Produccion::findorFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy($id)
     {
         //
